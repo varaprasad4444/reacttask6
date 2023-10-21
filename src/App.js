def83671faa572efa0f12d1d16e91abef3c44b34
N.VARA PRASAD <nethaladevi.saivara2021@vitstudent.ac.in>
2023-10-21 23:03:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Import your CSS file
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="portfolio">
+      <header>
+        <h1>Vara prasad</h1>
+        <p>Web Developer</p>
       </header>
+      <section className="projects">
+        <h2>Projects</h2>
+        {/* Map through and render your project components */}
+      </section>
+      <section className="skills">
+        <h2>Skills</h2>
+        {/* List your skills */}
+      </section>
+      <section className="experience">
+        <h2>Experience</h2>
+        {/* List your work experience */}
+      </section>
+      <section className="education">
+        <h2>Education</h2>
+        {/* List your educational background */}
+      </section>
+      <section className="contact">
+        <h2>Contact</h2>
+        {/* Add your contact information */}
+      </section>
     </div>
   );
-}
+};
 
 export default App;
